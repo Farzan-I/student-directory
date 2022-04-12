a2b0f139861b2799adfadec48ef59d8937bf3fac
Farzan-I <farzan_15@hotmail.co.uk>
2022-04-12 18:57:05 +0100

    Step 10: Rearranged methods

--- directory.rb
+++ directory.rb
@@ -1,22 +1,24 @@
 @students = []
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  
+  name = gets.chomp
+  
+  while !name.empty? do
+    
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    
+    name = gets.chomp
+  end
+end
+
 def interactive_menu
   loop do
     print_menu
     process(gets.chomp)
-  end
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
   end
 end
 
@@ -32,21 +34,17 @@ def show_students
   print_footer
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  
-  name = gets.chomp
-  
-  while !name.empty? do
-    
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    
-    name = gets.chomp
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
   end
-  
-  @students
 end
 
 def print_header
