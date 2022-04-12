4feaa785e92f14ff5f85fadd44184e70196ab778
Farzan-I <farzan_15@hotmail.co.uk>
2022-04-12 09:48:37 +0100

    Exercise 1

--- directory.rb
+++ directory.rb
@@ -1,19 +1,24 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+  
   students = []
-  # get the first name
+  
   name = gets.chomp
-  # while the name is not empty, repeat this code
+  
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    # Capitalised November
+    students << { name: name, cohort: :November }
+
+      # Made this to read better for one student
+      if students.count == 1
+        puts "Now we have #{students.count} student"
+      else
+        puts "Now we have #{students.count} students"
+      end
     name = gets.chomp
   end
-  # return the array of students
+ 
   students
 end
 
@@ -23,8 +28,9 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  # Exercise 1, the puts students with index and cohort
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
