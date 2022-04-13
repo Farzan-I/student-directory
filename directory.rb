fbbabbf380b2d4bca1c7c485fc2fcee0f39a26b4
Farzan-I <farzan_15@hotmail.co.uk>
2022-04-13 15:20:37 +0100

    Step 14: Ex. 1 (refactor @students)

--- directory.rb
+++ directory.rb
@@ -1,44 +1,7 @@
 @students = []
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  
-  name = gets.chomp
-  
-  while !name.empty? do
-    
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    
-    name = gets.chomp
-  end
-end
-
-def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-      @students << { name: name, cohort: cohort.to_sym }
-  end
-  file.close
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
+def students_instance(name, cohort = "november")
+  @students << { name: name, cohort: cohort.to_sym }
 end
 
 def print_menu 
@@ -49,10 +12,11 @@ def print_menu
   puts "9. Exit"
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
 end
 
 def process(selection)
@@ -72,6 +36,27 @@ def process(selection)
   end
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  
+  name = STDIN.gets.chomp
+  
+  while !name.empty? do
+    
+    students_instance(name)
+    puts "Now we have #{@students.count} students"
+    
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -87,4 +72,36 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    students_instance(name, cohort)
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
