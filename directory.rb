5bf320b7a6c8bd6322a2b400f7e69372bc684336
Farzan-I <farzan_15@hotmail.co.uk>
2022-04-14 13:26:22 +0100

    Step 14: Ex. 3 (Refactor)

--- directory.rb
+++ directory.rb
@@ -1,15 +1,13 @@
 @students = []
 
-def students_instance(name, cohort = "november")
+def students_instance(name, cohort = "November")
   @students << { name: name, cohort: cohort.to_sym }
 end
 
 def print_menu 
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
+  puts [" ", "1. Input the students", "2. Show the students", 
+  "3. Save the list to students.csv", "4. Load the list from students.csv", 
+  "9. Exit", " "]
 end
 
 def interactive_menu
@@ -21,24 +19,18 @@ end
 
 def process(selection)
   case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
+    when "1" then input_students
+    when "2" then show_students
+    when "3" then save_students
+    when "4" then load_students
+    when "9" then exit
+    else 
       puts "I don't know what you meant, try again"
   end
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts ["Please enter the names of the students", "To finish, just hit return twice"]
   
   name = STDIN.gets.chomp
   
@@ -46,7 +38,7 @@ def input_students
     
     students_instance(name)
     puts "Now we have #{@students.count} students"
-    
+
     name = STDIN.gets.chomp
   end
 end
@@ -58,8 +50,7 @@ def show_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts ["The students of Villains Academy", "--------------------------------"]
 end
 
 def print_students_list
